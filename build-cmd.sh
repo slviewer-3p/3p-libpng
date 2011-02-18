07922854d56de2c064770eb443a8bc1f8ea40920
Nick Twyman <squire@lindenlab.com>
2011-02-17 18:19:30 -0800

    Initial setup of autobuild files

---
+++ build-cmd.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+# turn on verbose debugging output for parabuild logs.
+set -x
+# make errors fatal
+set -e
+
+PNG_VERSION="1.5.1"
+PNG_SOURCE_DIR="libpng-$ZLIB_VERSION"
+
+if [ -z "$AUTOBUILD" ] ; then 
+    fail
+fi
+
+if [ "$OSTYPE" = "cygwin" ] ; then
+    export AUTOBUILD="$(cygpath -u $AUTOBUILD)"
+fi
+
+# load autbuild provided shell functions and variables
+set +x
+eval "$("$AUTOBUILD" source_environment)"
+set -x
+
+stage="$(pwd)/stage"
+pushd "$PNG_SOURCE_DIR"
+    case "$AUTOBUILD_PLATFORM" in
+        "windows")
+            load_vsvars
+            
+            pushd contrib/masmx86
+                ./bld_ml32.bat
+            popd
+            
+            build_sln "contrib/vstudio/vc10/zlibvc.sln" "Debug|Win32" "zlibstat"
+            build_sln "contrib/vstudio/vc10/zlibvc.sln" "Release|Win32" "zlibstat"
+            mkdir -p "$stage/lib/debug"
+            mkdir -p "$stage/lib/release"
+            cp "contrib/vstudio/vc10/x86/ZlibStatDebug/zlibstat.lib" \
+                "$stage/lib/debug/zlibd.lib"
+            cp "contrib/vstudio/vc10/x86/ZlibStatRelease/zlibstat.lib" \
+                "$stage/lib/release/zlib.lib"
+            mkdir -p "$stage/include/zlib"
+            cp {zlib.h,zconf.h} "$stage/include/zlib"
+        ;;
+        "darwin")
+            ./configure --prefix="$stage"
+            make
+            make install
+			mkdir -p "$stage/include/libpng"
+			mv "$stage/include/"*.h "$stage/include/libpng/"
+        ;;
+        "linux")
+            CFLAGS="-m32" CXXFLAGS="-m32" ./configure --prefix="$stage"
+            make
+            make install
+			mkdir -p "$stage/include/libpng"
+			mv "$stage/include/"*.h "$stage/include/libpng/"
+        ;;
+    esac
+    mkdir -p "$stage/LICENSES"
+    cp LICENSE "$stage/LICENSES/libgpng.txt"
+popd
+
+pass
+
