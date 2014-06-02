70bbeee13ee5547f947636da2d44c3403195d659
Monty Brandenberg <monty@lindenlab.com>
2014-06-02 11:58:23 -0400

    All:  Update zlib to 290556.  Mac:  Min OS version 10.6 for llqtwebkit2. Moving into the future.

--- build-cmd.sh
+++ build-cmd.sh
@@ -75,12 +75,11 @@ pushd "$PNG_SOURCE_DIR"
             # sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk/
             sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk/
 
-            # Keeping min version back at 10.5 because we may need to
-            # use this on the 10.5 build machine used for llqtwebkit.
+            # Keep min version back at 10.5 if you are using the
+            # old llqtwebkit repo which builds on 10.5 systems.
             # At 10.6, libpng will start using __bzero() which doesn't
-            # exist there.  Once we deal with legacy llqtwebkit, this 
-            # can bump up to 10.6.
-            opts="${TARGET_OPTS:--arch i386 -iwithsysroot $sdk -mmacosx-version-min=10.5}"
+            # exist there.
+            opts="${TARGET_OPTS:--arch i386 -iwithsysroot $sdk -mmacosx-version-min=10.6}"
 
             # Install name for dylibs (if we wanted to build them).
             # The outline of a dylib build is here disabled by '#dylib#' 
